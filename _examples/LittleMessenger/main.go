fd4d5e6c6985e782945ffc586171fed3f48d7d7d
XeshSufferer <3a4em_yt@mail.ru>
2026-02-21 04:49:47 +0200

    add examples

--- _examples/LittleMessenger/main.go
+++ _examples/LittleMessenger/main.go
@@ -1,7 +1,9 @@
-package LittleMessenger
+package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
 
 	"github.com/XeshSufferer/huclient"
 	"github.com/gorilla/websocket"
@@ -9,21 +11,38 @@ import (
 
 func main() {
 	// init
-	client := huclient.NewClient("example.com", "app")
+	client := huclient.NewClient("localhost:3000", "app")
 
 	client.On("ReceiveMessage", func(conn *websocket.Conn, message *huclient.Message) {
-		fmt.Println("Receive Message!")
-		fmt.Println(message.Args) // Write Raw JSON
+		fmt.Println(string(message.Args)) // Write Raw JSON
+	})
+
+	client.On("OnJoin", func(conn *websocket.Conn, message *huclient.Message) {
+		fmt.Println(string(message.Args))
+	})
+
+	client.On("OnLeave", func(conn *websocket.Conn, message *huclient.Message) {
+		fmt.Println(string(message.Args))
 	})
 
 	client.Connect()
 
-	for {
-		var input string
-		fmt.Scanln(&input)
+	fmt.Print("Enter nickname: ")
+	var nickname string
+
+	fmt.Scanln(&nickname)
+	err := client.SendMessage("Join", nickname)
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	scanner := bufio.NewScanner(os.Stdin)
+
+	for scanner.Scan() {
 
 		// Sending to server RPC "SendMessage" with arg = input
-		err := client.SendMessage("SendMessage", input)
+		err := client.SendMessage("SendMessage", scanner.Text())
 
 		if err != nil {
 			fmt.Println(err)
